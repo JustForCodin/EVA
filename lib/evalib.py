5b6d9310a2830dc882deb4cfb96679d0ec0c7668
JustForCoding <mrme9093@gmail.com>
2022-04-12 17:25:57 +0300

    moved to PyCharm

--- lib/evalib.py
+++ lib/evalib.py
@@ -1,27 +1,28 @@
 class EVADecision:
-    d: float   # decision rationality 
-    l: float   # decision label (e.g 0, 1 or -1)
+    d: float  # decision rationality
+    l: float  # decision label (e.g 0, 1 or -1)
 
     def __init__(self, d, l) -> None:
         self.d = d
         self.l = l
+
 
 class EVADecisionEvaluator:
     decision_space: list
 
     def __init__(self, decision_space: list):
         self.decision_space = decision_space
-    
+
     def decision_forward_pass(self):
         R = []
         for i in range(len(self.decision_space)):
-            R.append(self.decision_space[i]-self.decision_space[i-1])
-        return min(R)    
+            R.append(self.decision_space[i] - self.decision_space[i - 1])
+        return min(R)
 
     def decision_backward_pass(self):
         R = []
         for i in range(len(self.decision_space)):
-            R.append(self.decision_space[i-1]-self.decision_space[i])
+            R.append(self.decision_space[i - 1] - self.decision_space[i])
         return min(R)
 
     def decision_forward_pass_2d(self):
@@ -29,9 +30,9 @@ class EVADecisionEvaluator:
         for i in range(len(self.decision_space)):
             R.append(
                 [
-                    self.decision_space[i].d - self.decision_space[i-1].d,
+                    self.decision_space[i].d - self.decision_space[i - 1].d,
                     self.decision_space[i].l
-                ] 
+                ]
             )
         return min(R)
 
@@ -40,8 +41,8 @@ class EVADecisionEvaluator:
         for i in range(len(self.decision_space)):
             R.append(
                 [
-                    self.decision_space[i-1].d - self.decision_space[i].d,
+                    self.decision_space[i - 1].d - self.decision_space[i].d,
                     self.decision_space[i].l
-                ] 
+                ]
             )
         return min(R)
